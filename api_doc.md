3da752c1353fb62c0547b3ed993c33dce9ff8a21
= <=>
2025-03-31 12:15:01 +0800

    删除多余api

--- api_doc.md
+++ api_doc.md
@@ -1,21 +1,7 @@
-## 删除用户
-URL: /delete_user
-Method: post
-
-参数: user_id
-格式: json
-`{"id": <user_id> }`
-
-返回:<br>
-    `{"code": 0, "msg": "ok"}`<br>
-    `{"code": 1, "msg": "error"}`
-
-
 ## 通过用户名字获取用户信息
-
-URL: /get_user_by_name<br>
-method: get<br>
-参数: name<br>
+URL: `/get_users_by_name`<br>
+method: `get`<br>
+参数: `name`<br>
 返回:<br>
     `{"code": 200, "msg": "ok", "data": [...]}`<br>
     `{"code": 400, "msg": "not found"}`
@@ -29,3 +15,53 @@ method: get<br>
             "age": age
         }
 
+## 通过获取制定性别的用户
+URL: `/get_users_by_gender`<br>
+method: `get`<br>
+参数: `gender`<br>
+返回:<br>
+    `{"code": 200, "msg": "ok", "data": [...]}`
+```
+data包含用户对象    
+User对象结构:
+    {
+        "id": id,
+        "username": username,
+        "gender": gender,
+        "age": age
+    } 
+```
+
+## 通过获取制定年龄的用户
+URL: `/get_users_by_age`<br>
+method: `get`<br>
+参数: `age`<br>
+返回:<br>
+    `{"code": 200, "msg": "ok", "data": [...]}`
+ ```
+ data包含用户对象    
+ User对象结构:
+     {
+         "id": id,
+         "username": username,
+         "gender": gender,
+         "age": age
+     }
+```
+
+## 通过年龄范围获取用户
+URL: `/get_users_by_age_range`<br>
+method: `get`<br>
+参数: `age_min`, `age_max`
+返回:<br>
+    `{"code": 200, "msg": "ok", "data": [...]}`
+```
+data包含用户对象    
+User对象结构:
+    {
+        "id": id,
+        "username": username,
+        "gender": gender,
+        "age": age
+    }
+```
--- views.py
+++ views.py
@@ -7,7 +7,7 @@ def index():
     return render_template("user_query.html")
 
 @app.route("/get_users_by_name", methods=["GET"])
-def getUser():
+def getUserByName():
     data = {
         "code": "200",
         "msg": "success",
@@ -30,63 +30,7 @@ def getUser():
     data["data"] = [user.to_dict() for user in users]
     return data
     
-
-@app.route("/add_user", methods=["POST"])
-def addUser():
-    data = {
-        "code": "200",
-        "msg": "success",
-        "data": []
-    }
-
-    name = request.form.get("name")
-    age = request.form.get("age")
-    gender = request.form.get("gender")
-
-    if not name:
-        data["code"] = "400"
-        data["msg"] = "name is required"
-        return data
-    
-    if not age:
-        data["code"] = "400"
-        data["msg"] = "age is required"
-        return data
-    
-    if not gender:
-        data["code"] = "400"
-        data["msg"] = "gender is required"
-        return data
-    
-    user = User(name=name, age=age, gender=gender)
-    user.save()
-    return data
-
-@app.route("/update_user", methods=["POST"])
-def updateUser():
-    data = {
-        "code": "200",
-        "msg": "success",
-        "data": []
-    }
-    id = request.form.get("id")
-    name = request.form.get("name")
-    age = request.form.get("age")
-    gender = request.form.get("gender")
-    if not id:
-        data["code"] = "400"
-        data["msg"] = "id is required"
-        return data
-    
-    user = User.findUserById(id)
-    if not user:
-        data["code"] = "400"
-        data["msg"] = "user not found"
-        return data
-    
-
 @app.route("/get_users_by_gender", methods=["GET"])
-
 def getUserByGender():
     data = {
         "code": "200",
@@ -99,11 +43,48 @@ def getUserByGender():
         data["msg"] = "gender is required"
         return data
     
-    users = User.findUsersByGender(gender)
-    if not users:
+    if gender not in [0, 1]:
         data["code"] = "400"
-        data["msg"] = "user not found"
+        data["msg"] = "gender is invalid"
         return data
     
-    data["data"] = users
+    data["data"] = [user.to_dict() for user in User.findUsersByGender(gender)]
+    return data
+
+@app.route("/get_users_by_age", methods=["GET"])
+def getUserByAge():
+    data = {
+        "code": "200",
+        "msg": "success",
+        "data": []
+    }
+
+    age = request.args.get("age")
+    if not age:
+        data["code"] = "400"
+        data["msg"] = "age is required"
+        return data
+    
+    users = User.query.filter_by(age=age).all()
+    data["data"] = [user.to_dict() for user in users]
+    return data
+
+@app.route("/get_users_by_age_range", methods=["GET"])
+def getUserByAgeRange():
+    data = {
+        "code": "200",
+        "msg": "success",
+        "data": []
+    }
+    age_min = request.args.get("age_min")
+    age_max = request.args.get("age_max")
+
+    if not age_min or not age_max:
+        data["code"] = "400"
+        data["msg"] = "age_min and age_max are required"
+        return data
+    
+    users = User.query.filter(User.age >= age_min, User.age <= age_max).all()
+    
+    data["data"] = [user.to_dict() for user in users]
     return data
